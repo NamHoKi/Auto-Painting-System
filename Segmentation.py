256f5156461a454df7f9dfb59a8ad27adcb962ca
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2020-02-27 04:51:24 +0900

    세그멘테이션

--- Segmentation.py
+++ Segmentation.py
@@ -1,7 +1,66 @@
 import cv2
-import numpy as np
+import copy
 
-def segmentation(img):
-  pass
+class Segmentation(object):
+    def start(self):
+        pass
 
-## bfs로 모든픽셀 검사 후 그루핑
+    def test(self):
+        file_name = input('$ File name : ')
+        img = cv2.imread(file_name,0)
+        img = self.binarize(img,220)
+        sg = self.segmentation(img)
+        origin = cv2.imread(file_name)
+        test = self.segmentation_image_show(origin,sg[0],sg[1])
+        cv2.imshow('Origign',origin)
+        cv2.imshow('Color',test)
+        cv2.waitKey(0)
+
+    def binarize(self, img, threshold):
+        # 이진화
+        ret, bin_img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+        return bin_img
+
+    def segmentation(self, img):
+        segmentation_img = copy.deepcopy(img)
+        offset = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        count = 0
+        start_point = []
+
+        for i in range(len(img)):
+            for j in range(len(img[0])):
+                if segmentation_img[i][j] == 255:
+                    start_point.append([i,j])
+                    count += 1
+                    q = [[i,j]]
+                    while q:
+                        cur = q.pop(0)
+                        x, y = cur[0], cur[1]
+                        if x < 0 or y < 0:
+                            pass
+                        elif x > len(img) - 1 or y > len(img[0]) - 1:
+                            pass
+                        elif segmentation_img[x][y] != 255:
+                            pass
+                        else:
+                            segmentation_img[x][y] = count
+                            for i in range(4):
+                                q.append([x + offset[i][0], y + offset[i][1]])
+        return [segmentation_img, count]
+
+    def segmentation_image_show(self,origin_img, segmentation_img, count):
+        color_img = copy.deepcopy(origin_img)
+        print(count)
+        if count <= 5:
+            color = [0,0,255]
+        else:
+            color = [200,0,0]
+
+        for i in range(len(segmentation_img)):
+            for j in range(len(segmentation_img[0])):
+                if segmentation_img[i][j] != 0 and segmentation_img[i][j] != 255 and segmentation_img[i][j] != 1:
+                    color_img[i][j] = color
+        return color_img
+
+sg = Segmentation()
+sg.test()
