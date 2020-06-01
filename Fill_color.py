73f2d73f3509b831ff3cca019a2a32f884d11a45
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2020-06-01 18:23:36 +0900

    채색

--- Fill_color.py
+++ Fill_color.py
@@ -30,6 +30,82 @@ class Fill_color(object):
         ret, bin_img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
         return bin_img
 
+    # def segmentation(self, img):
+    #     segmentation_img = copy.deepcopy(img)
+    #     offset = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    #     count = 0
+    #     start_point = []
+    #
+    #     for i in range(len(img)):
+    #         for j in range(len(img[0])):
+    #             if segmentation_img[i][j] == 255:
+    #                 start_point.append([i, j])
+    #                 count += 1
+    #                 q = [[i, j]]
+    #                 while q:
+    #                     cur = q.pop(0)
+    #                     x, y = cur[0], cur[1]
+    #                     if x < 0 or y < 0:
+    #                         pass
+    #                     elif x > len(img) - 1 or y > len(img[0]) - 1:
+    #                         pass
+    #                     elif segmentation_img[x][y] != 255:
+    #                         pass
+    #                     else:
+    #                         segmentation_img[x][y] = count
+    #                         for i in range(4):
+    #                             q.append([x + offset[i][0], y + offset[i][1]])
+    #     return segmentation_img
+    #
+    # def segmentation_image_show(self, origin_img, segmentation_img, color, xy):
+    #     color_img = copy.deepcopy(origin_img)
+    #     color_count = segmentation_img[xy[0]][xy[1]]
+    #
+    #     for i in range(len(segmentation_img)):
+    #         for j in range(len(segmentation_img[0])):
+    #             if segmentation_img[i][j] != 0 and segmentation_img[i][j] != 255 and segmentation_img[i][j] == color_count:
+    #                 color_img[i][j] = color
+    #     self.natual_coloring(color_img,50)
+    #     return color_img
+    #
+    # def natual_coloring(self, img, value):
+    #     random_num = random.randrange(125,175)
+    #     for i in range(random_num-value,random_num+value):
+    #         for j in range(random_num-value,random_num+value):
+    #             d = self.p2p_dst(i,j,random_num,random_num)
+    #             if d <= value and self.img2np(img[i][j],[0,0,0]) and self.img2np(img[i][j],[255,255,255]):
+    #                 for k in range(0,3):
+    #                     img[i][j][k] = self.check255(img[i][j][k] + value - d)
+    #     # img = cv2.GaussianBlur(img, (11, 11), 0)
+    #
+    # def p2p_dst(self,x1,y1,x2,y2):
+    #     return int(math.sqrt((x2-x1)**2 + (y2-y1)**2))
+    #
+    # def img2np(self,v1,v2):
+    #     if v1[0] == v2[0] and v1[1] == v2[1] and v1[2] == v2[2]:
+    #         return False
+    #     return True
+    #
+    # def check255(self,v):
+    #     if v >= 255:
+    #         return 255
+    #     return v
+    #
+    # def filter(self, img):
+    #     ft = cv2.imread('ra.png')
+    #     for i in range(0,len(img)):
+    #         for j in range(0,len(img[0])):
+    #             if [img[i][j][0],img[i][j][1],img[i][j][2]] !=[255,255,255] and [img[i][j][0],img[i][j][1],img[i][j][2]] !=[0,0,0]:
+    #                 # 명도 사용하기
+    #                 img[i][j][0] = img[i][j][0] + ft[i][j][0]
+    #                 img[i][j][1] = img[i][j][1] + ft[i][j][1]
+    #                 img[i][j][2] = img[i][j][2] + ft[i][j][2]
+    #
+    #                 ## 그대로 가져오기
+    #                 # img[i][j][0] = ft[i][j][0]
+    #                 # img[i][j][1] = ft[i][j][1]
+    #                 # img[i][j][2] = ft[i][j][2]
+
     def segmentation(self, img):
         segmentation_img = copy.deepcopy(img)
         offset = [[1, 0], [0, 1], [-1, 0], [0, -1]]
@@ -115,15 +191,20 @@ class Fill_color(object):
                             color_img[i][j] = color[seg_cnt]
         elif label == 'flower':
             # 꽃
-            color = [0,212,255]
-            for seg_cnt in range(1):
+            center_circle = segmentation_img[150][150]
+            color = [0,255,255]
+            for seg_cnt in range(count - 1):
                 for i in range(len(segmentation_img)):
                     for j in range(len(segmentation_img[0])):
                         if segmentation_img[i][j] == color_count[seg_cnt]:
-                            color_img[i][j] = color[seg_cnt]
+                            color_img[i][j] = color
+            for i in range(0,300):
+                for j in range(0,300):
+                    if segmentation_img[i][j] == center_circle:
+                        color_img[i][j] = [0,0,160]
         elif label == 'leaf':
             # 잎
-            color = [0,180,0]
+            color = [20,160,20]
             for i in range(len(segmentation_img)):
                 for j in range(len(segmentation_img[0])):
                     if segmentation_img[i][j] != 0 and segmentation_img[i][j] != 255 and segmentation_img[i][j] != 1:
@@ -137,12 +218,15 @@ class Fill_color(object):
                         color_img[i][j] = color
         elif label == 'carrot':
             # 당근
-            color = [[0,0,255],[0,255,0]]
-            for seg_cnt in range(2):
+            color = [[38,67,243],[10,180,10]]
+            for seg_cnt in range(count - 1):
                 for i in range(len(segmentation_img)):
                     for j in range(len(segmentation_img[0])):
                         if segmentation_img[i][j] == color_count[seg_cnt]:
-                            color_img[i][j] = color[seg_cnt]
+                            if len(color)-1 <= seg_cnt:
+                                color_img[i][j] = color[-1]
+                            else:
+                                color_img[i][j] = color[seg_cnt]
         return color_img
 
     def return_size(self,img, return_num):
@@ -182,6 +266,17 @@ class Fill_color(object):
         if v >= 255:
             return 255
         return v
+
+    # def add_shadow(self, img):
+    #     histogram = [0] * len(img)
+    #     line = len(img)
+    #     for i in range(len(img),1,-1):
+    #         for j in range(len(img[0])):
+    #
+    #
+    #     for i in range(len(img)):
+    #         for j in range'나,숟기(len(img[0])):
+    #             pass
 
     def line_effect(self, seg_img, color_img, value, n):
         for i in range(len(seg_img)):
